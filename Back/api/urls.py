f7b4e269db916f82424e83f5aa2347da1a2872c0
root <root@WIN-7E8AJGMQ4BP.localdomain>
2025-10-27 04:53:56 +0300

    hotfix: fix import bug

--- Back/api/urls.py
+++ Back/api/urls.py
@@ -1,21 +1,19 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from api.views import api_root
 from trips.views import TripViewSet
 from gallery.views import GalleryViewSet
 from reviews.views import ReviewViewSet
 from contact.views import ContactInfoViewSet, ContactMessageViewSet
 
-# Create a router and register our viewsets with it
+# Create a router and register our viewsets with it.
 router = DefaultRouter()
 router.register(r'trips', TripViewSet, basename='trip')
 router.register(r'gallery', GalleryViewSet, basename='gallery')
 router.register(r'reviews', ReviewViewSet, basename='review')
-router.register(r'contact', ContactInfoViewSet, basename='contact')
+router.register(r'contact-info', ContactInfoViewSet, basename='contact-info')
 router.register(r'messages', ContactMessageViewSet, basename='message')
 
-# The API URLs are determined automatically by the router
+# The API URLs are determined automatically by the router.
 urlpatterns = [
-    path('', api_root, name='api-root'),
     path('', include(router.urls)),
 ]
